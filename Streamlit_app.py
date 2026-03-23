357024ed438866c3cba26dad26a0019cdaf7379e
laks-max <Lakshmi.Ponduri@endava.com>
2026-03-23 12:16:07 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,10 +1,10 @@
 # Import python packages
 
+from snowflake.snowpark.functions import col
 import streamlit as st
 
 #from snowflake.snowpark.context import get_active_session
 
-from snowflake.snowpark.functions import col
 
 import requests
  
